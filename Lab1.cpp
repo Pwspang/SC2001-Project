f55e56a9a29e44e351fa611a88360365f8d3b95c
shuangshuu <tayshushuang@gmail.com>
2023-09-06 15:34:13 +0800

    comparisons

--- Lab1.cpp
+++ Lab1.cpp
@@ -26,14 +26,14 @@ using namespace std;
 #define getS(x) scanf("%s", x) //get a char* string
 #define clr(a,x) memset(a,x,sizeof(a)) //set elements of array to some value
 #define char2Int(c) (c-'0')
-#define lastEle(vec) vec[vec.size()-1] 
+#define lastEle(vec) vec[vec.size()-1]
 #define SZ(x) ((int)((x).size()))
 #define REMAX(a,b) (a)=max((a),(b)) // set a to the maximum of a and b
 #define REMIN(a,b) (a)=min((a),(b));
 #define FOREACH(i,t) for (typeof(t.begin()) i=t.begin(); i!=t.end(); i++) // traverse an STL data structure
 #define ALL(c) (c).begin(),(c).end() //handy for function like "sort()"
-#define PRESENT(c,x) ((c).find(x) != (c).end()) 
-#define CPRESENT(c,x) (find(ALL(c),x) != (c).end()) 
+#define PRESENT(c,x) ((c).find(x) != (c).end())
+#define CPRESENT(c,x) (find(ALL(c),x) != (c).end())
 #define ll long long //data types used often, but you don't want to type them time by time
 #define ull unsigned long long
 #define ui unsigned int
@@ -54,16 +54,16 @@ inline void pisz(int n) { printf("%d\n",n); }
 //for vectors
 #define pb push_back
 typedef int elem_t;
-typedef vector<int> vi; 
-typedef vector<vi> vvi; 
-typedef pair<int,int> ii; 
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> ii;
 // directions
 const int fx[4][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
 const int fxx[8][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
 template<typename T,typename TT> ostream& operator<<(ostream &s,pair<T,TT> t) {return s<<"("<<t.first<<","<<t.second<<")";}
 template<typename T> ostream& operator<<(ostream &s,vector<T> t){F(i,0,SZ(t))s<<t[i]<<" ";return s; }
 
- 
+
 vi v;
 ull n_comp=0;
 
@@ -78,14 +78,14 @@ int comp(int a, int b){
 void insertionSort(int l, int r){
 	FE(i,l+1,r){
 		for (int j=i; j > l; j--){
-			if (comp(j, j-1)==1) swap(v[j], v[j-1]);
+			if (comp(v[j], v[j-1])==1) swap(v[j], v[j-1]);
 			else break;
 		}
-	}	
+	}
 }
 
 void mergeSort(int l, int r, int s){
-	// ending 
+	// ending
 	if (r-l+1 <= s) insertionSort(l,r);
 	else {
 		int m=l+r/2;
@@ -119,7 +119,7 @@ void mergeSort(int l, int r, int s){
 int32_t main(){
 	ios_base::sync_with_stdio(false), cin.tie(nullptr);
 	int n,s;
-	
+
 	// cout << "Enter size: ";
 	cin >> n;
 	v.resize(n);
